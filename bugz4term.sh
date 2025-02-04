47a22b7182ffe83d59ab1da78d70d28ebaa5c983
root <vikramzeus98@gmail.com>
2025-02-04 21:42:04 +0530

    Added custom ffuf placeholder commands in .bugs4term.conf

--- bugz4term.sh
+++ bugz4term.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # This script allows for creating 4 terminator windows that are loaded with the user specified commands. The command will be defaulted to creating 4 terminator windows each loaded with user specified commands in each terminator window ready to be executed when time is a concern in bug bounty or any specific methodology since you are able to have custom commands loaded.
+
+domain=""
+
 usage(){
 	cat <<EOF
 	Usage: ./bugs4term.sh [options]
-		-c : Run bugs4term from the bugs4term cofiguration file
+		-c : Run from bugs4term cofig file
+		-h : Help section
 EOF
 }
 
@@ -21,8 +25,23 @@ loadRunConfig(){
 	$(xdotool type "$(sed -rn '4p' "$configFile")")
 }
 
-while getopts ":c:" OPTS; do
+while getopts ":c:hd:" OPTS; do
 	case "$OPTS" in
+		d)
+			testDomain="${OPTARG}"
+			grep -Eiwq "^(https?:\/\/)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" <<< "$testDomain"
+			if [[ $? -eq 0 ]]; then
+				if [[ -n "$bugs4termConfigFile" ]] && [[ -f "$bugs4termConfigFile" ]]; then
+					echo "file exists"
+				else
+					echo "Mention -c [config] flag as well. Exiting."
+					exit 1
+				fi
+			else
+				echo "Not a domain"
+				exit 1
+			fi
+			;;
 		c)
 			bugs4termConfigFile=".bugs4term.conf"
 			if [[ -f "$OPTARG" ]] && [[ "$OPTARG" =~ "$bugs4termConfigFile" ]] ; then
@@ -33,6 +52,11 @@ while getopts ":c:" OPTS; do
 				echo "Is not .bugs4term.conf file"
 				exit 1
 			fi
+			;;
+
+		h)
+			usage
+			exit 1
 			;;
 		\?)
 			echo "Invalid option"
